e3225bd54de9a74328ca9a31b2d11c94554a276f
Tucker K <tmk1270@rit.edu>
2023-01-18 11:54:43 -0500

    Attempt 2

---
+++ FavoriteColorAndNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteColorAndNumber  //Its a school - Container for classes
+{   /* (Always have this block before each class)
+    Class: Program
+    Author: Tucker Knapman
+    Purpose: Console Read/Write and Exception handling exercise
+    Restrictions: None
+    */
+    static internal class Program {  //Class as used for whatnot ---
+                                    //Static here makes life easier for pregenerated stuff
+        static void Main(string[] args)
+        {
+        }
+    }
+}
