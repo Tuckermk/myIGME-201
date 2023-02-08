29fd41a541d4c4c9077794e59b52199b951c0a8d
Tucker K <tmk1270@rit.edu>
2023-02-08 00:59:34 -0500

    Question 7 Comment BLocks

--- HW8/HW8-7/Program.cs
+++ HW8/HW8-7/Program.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace HW8_7
 {
+    /* Class : Program
+    Author : Tucker Knapman
+    Purpose : Reverse the inputted string
+    Restricitions: None */
     internal class Program
     {
+        /* Method: Main
+         * Purpose: Run the solution to the question
+            Restrictions: None */
         static void Main(string[] args)
         {
             Console.WriteLine("Write a thing to be reversed");
