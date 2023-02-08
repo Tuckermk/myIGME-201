11b754aefb48a5e8c36e488303c834471169ad9f
Tucker K <tmk1270@rit.edu>
2023-02-08 00:29:45 -0500

    Question 7

--- HW8-5/Program.cs
+++ HW8-5/Program.cs
@@ -14,21 +14,22 @@ namespace HW8_5
             double x;
             double y;
             double z;
-            superArray = new double[31,41,50];
-            for(x = -1; x <= 1; x+=.1)
+            superArray = new double[31, 41, 50];
+            for (x = -1; x <= 1; x += .1)
             {
-               
+
                 for (y = 1; y <= 4; y += .1)
                 {
-                    
+
                     z = 3 * Math.Pow(y, 2) + (2 * x) - 1;
-                    int xInt = (int)(x*10)+10;
-                    int yInt = (int)(y*10);
+                    int xInt = (int)(x * 10) + 10;
+                    int yInt = (int)(y * 10);
                     int zInt = (int)z;
-                    
+
                     superArray[xInt, yInt, zInt] = z;
                 }
             }
+
         }
     }
 }
---
+++ HW8-7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW8_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a thing to be reversed");
+            string input = Console.ReadLine();
+            char[] inputArray = input.ToCharArray();
+            Array.Reverse(inputArray);
+            
+            Console.WriteLine(inputArray);
+        }
+    }
+}
