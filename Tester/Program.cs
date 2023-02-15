3297741b46836ad858d4638e48c425431e583ab8
Tucker K <tmk1270@rit.edu>
2023-02-14 22:35:56 -0500

    Question 2

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,55 +1,97 @@
-﻿using System;
+﻿using System.Timers;
+using System;
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+
+
+/// delegate steps
+/// 1. define the delegate method data type based on the method signature
+///         delegate double MathFunction(double n1, double n2);
+/// 2. declare the delegate method variable
+///         MathFunction processDivMult;
+/// 3. point the variable to the method that it should call
+///         processDivMult = new MathFunction(Multiply);
+/// 4. call the delegate method / add the delegate method to the object's event handler
+///         nAnswer = processDivMult(n1, n2);
+///         timer.Elapsed += TimesUp;
 
 namespace Tester
 {
-    internal class Program
+    static internal class Program
     {
+        static Timer timeOutTimer;
+        static bool bTimeOut = false;
+
         static void Main(string[] args)
         {
 
 
-            int[] ascendingNumbers = new int[5];
-            for (int i = 0; i < ascendingNumbers.Length; i++)
+        start:
+            bTimeOut = false;
+
+            string displayString = "";
+            Random rand = new Random();
+            Console.Clear();
+
+            while (!bTimeOut)
             {
-                ascendingNumbers[i] = i + 1;
+                displayString += (char)('A' + rand.Next(0, 26));
+                foreach (char c in displayString)
+                {
+                    Console.Write(c);
+                    System.Threading.Thread.Sleep(500);
+                }
+
+                Console.Clear();
+
+                timeOutTimer = new Timer(displayString.Length * 500 + 1000);
+
+                // Timer calls the Timer.Elapsed event handler when the time elapses
+                // The Timer.Elapsed event handler uses a delegate function with the following signature:
+                //        public delegate void ElapsedEventHandler(object sender, ElapsedEventArgs e);
+                // This delegate method type is already defined for us by .NET
+                timeOutTimer.Elapsed += new ElapsedEventHandler(TimesUp);
+
+                timeOutTimer.Start();
+
+                string sAnswer = null;
+                sAnswer = Console.ReadLine();
+
+                timeOutTimer.Stop();
+
+                if (sAnswer.ToUpper() == displayString && !bTimeOut /* same as bTimeOut == false */ )
+                {
+                    Console.WriteLine("Well Done!  Your current score is {0}", displayString.Length);
+                }
+                else
+                {
+                    Console.WriteLine("Bad luck.  :(  The correct code was {0}.  Your final score is: {1}", displayString, displayString.Length - 1);
+
+                    // set timeout to leave the while() loop
+                    bTimeOut = true;
+                }
             }
 
+            Console.Write("Press Enter to Play Again");
+            Console.ReadLine();
 
-
-            double[] percipitation;
-            percipitation = new double[4];
-            percipitation[0] = .45;
-            percipitation[1] = 2.78;
-            percipitation[2] = .04;
-            percipitation[3] = 1.22;
-
-
+            goto start;
 
         }
-        enum colors : ushort
+
+        static void TimesUp(object sender, ElapsedEventArgs e)
         {
-            red,
-            orange,
-            yellow,
-            green,
-            blue,
-            purple,
-            black,
-            white
+            Console.WriteLine();
+            Console.WriteLine("Your time is up!");
+            Console.WriteLine("Please press enter");
+            bTimeOut = true;
+            timeOutTimer.Stop();
         }
-        static float test()
-    {
-
-        byte x = 0;
-        return x;
     }
-
-
-
-    } 
 }
-
