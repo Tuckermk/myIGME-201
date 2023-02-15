97bcbad49add4a3efcc7867c42b1e9c889ca3f88
Tucker K <tmk1270@rit.edu>
2023-02-14 22:49:07 -0500

    Question 5

---
+++ HW9/Question5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question5
+{
+    public delegate string ReadLine();
+    
+
+internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           ReadLine deli = new ReadLine(ReadLineImpersonator);
+        }
+        static string ReadLineImpersonator()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
