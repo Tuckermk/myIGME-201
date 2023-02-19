40d49b6cee50e4b79fceca8c1261feb2ef615ea9
Tucker K <tmk1270@rit.edu>
2023-02-19 16:52:18 -0500

    Test 1 question 12

---
+++ Test1/Test1-12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sName;
+            double dSalary = 30000;
+
+            Console.WriteLine("Input name");
+            sName = Console.ReadLine();
+            GiveRaise(sName, ref dSalary);
+        }
+        static bool GiveRaise(string name, ref double salary)
+        {
+            if (name.ToLower() != "tucker")
+            {
+                return false;
+            }
+
+            salary += 19999.99;
+            Console.WriteLine("Congrats on payraise");
+            return true;
+        }
+    }
+}
--- Tester/Program.cs
+++ Tester/Program.cs
@@ -7,55 +7,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Timers;
 
 
-/// delegate steps
-/// 1. define the delegate method data type based on the method signature
-///         delegate double MathFunction(double n1, double n2);
-/// 2. declare the delegate method variable
-///         MathFunction processDivMult;
-/// 3. point the variable to the method that it should call
-///         processDivMult = new MathFunction(Multiply);
-/// 4. call the delegate method / add the delegate method to the object's event handler
-///         nAnswer = processDivMult(n1, n2);
-///         timer.Elapsed += TimesUp;
+
+
 
 namespace Tester
 {
-    using When = pain;
- 
-    static internal class Program
-    {
-       
-        static void Main(string[] args)
-        {
-            float value;
-            
-        }
-    }
-    namespace pain
-    {
-        
-        static public class miniPro
-        {
-            
-            static string Second()
-            {
-                Program();
-            }
-        }
-    }
-}
-namespace Worst
-{
-    using When = Episode;
-    // name "Meme" defined
+    class Program {
+        static void Main(string[] args) {
+            uint hi = 2333333333;
+            long bye = hi;
 
-    namespace Episode
-    {
-        // name "Ever" defined
-        
-        static public string Ever;
+
+        }
     }
 }
