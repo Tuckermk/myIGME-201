5c498ed205397c02cd977b1353ecb275072e8938
Tucker K <tmk1270@rit.edu>
2023-02-19 17:00:11 -0500

    Test 1 Question 13

---
+++ Test1/Test1-13/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_13
+{
+    internal class Program
+    {
+        struct employee
+        {
+            public string sName;
+            public double dSalary;
+        }
+        static void Main(string[] args)
+        {
+            string sName;
+            double dSalary = 30000;
+
+            employee sEmployee = new employee();
+            Console.WriteLine("Input name");
+            sEmployee.sName = Console.ReadLine();         
+
+            GiveRaise(sEmployee);
+        }
+        static bool GiveRaise(employee employ)
+        {
+            if (employ.sName.ToLower() != "tucker")
+            {
+                return false;
+            }
+
+            employ.dSalary += 19999.99;
+            Console.WriteLine("Congrats on payraise");
+            return true;
+        }
+    }
+}
